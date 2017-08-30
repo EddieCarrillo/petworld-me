9eb16b640ceebdcfec737c1cd993eafcbfa630f9
EddieCurio <edcarril@ucsd.edu>
2017-08-29 18:14:41 -0700

    Added sign up API call to server. Passed tests.

--- PetWorld/UserAPI.swift
+++ PetWorld/UserAPI.swift
@@ -12,37 +12,68 @@ import Foundation
 class UserAPI{
     
     
-    var username: String?
-    var email: String?
+   static var postMethod = "POST"
     
-    let path = "/users"
+    static let path = "/users"
+    static let contentType = "Content-Type"
+    static let applicationJson = "application/json"
    
     
     
-    class func signUp(new user: User, password: String, onFinished: @escaping (String?,Error? ) -> Void){
-        let url = URL(string: "\(NetworkAPI.apiBaseUrl)/(path)")
+    class func signUp(new user: User, onFinished: @escaping (String?,Error? ) -> Void){
+        let url = URL(string: "\(NetworkAPI.apiBaseUrl)\(path)")
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
-        let urlRequest = URLRequest(url: url!)
+        var urlRequest = URLRequest(url: url!)
+        var requestUserBody = user.toJson()
+        print("json string: \(requestUserBody)")
+        
+        urlRequest.httpMethod = postMethod
+        urlRequest.addValue(applicationJson, forHTTPHeaderField: contentType)
+        
+        if let jsonBody = requestUserBody{
+            urlRequest.httpBody = jsonBody
+            print(urlRequest)
+        }
+        
+        
+        print("Task to be called!")
         
         let task = session.dataTask(with: urlRequest) { (data: Data?, response: URLResponse?, error :Error?) in
+            
             if let error = error{
+                print("Error")
                 onFinished(nil, error)
             }else if let response = response{
                 let response = response as! HTTPURLResponse
+                
                 print("RESPONSE: \(response)")
                 let code = response.statusCode
-                if (code != 200 || code != 201){
+                if (code != 200 && code != 201){
                     let error = NSError(domain: "Bad request", code: code, userInfo: nil)
                     onFinished(nil, error)
                 }else if let data = data{
-                    let token = String.init(data: data, encoding: String.Encoding.utf8)
-                    onFinished(token, nil)
+                    var jsonBody: [String: Any]?
+                    do{
+                        
+                        jsonBody = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                    }catch{
+                        onFinished(nil, NSError(domain: "Could not deserialize json", code: 404, userInfo: nil))
+                        
+                    }
+                    
+                    if let jsonBody = jsonBody{
+                        var token = jsonBody["token"] as? String
+                        onFinished(token, nil)
+                    }else {
+                        onFinished(nil, NSError(domain: "Could not deserialize json", code: 404, userInfo: nil) )
+                    }
+ 
                 }
         }
-        
-    
     }
+        
+        task.resume()
         
         
         

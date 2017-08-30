c6110ef54b33a88a91675a0f23967c8d32a6de93
EddieCurio <edcarril@ucsd.edu>
2017-08-29 18:15:03 -0700

    Added tests for signing up user.

--- PetWorldTests/PetWorldTests.swift
+++ PetWorldTests/PetWorldTests.swift
@@ -22,16 +22,84 @@ class PetWorldTests: XCTestCase {
         super.tearDown()
     }
     
-    func testUser(){
-        let user = User(username: "Eddstah", email: "eddstah@hotmail.com")
-      let data =  user.toJson(with: "mypassword")
+    func testUserToJson(){
+        let user = User(username: "Eddstah", email: "eddstah@hotmail.com", password: "pass")
+      let data =  user.toJson()
+       
         
+        var jsonBody: [String: Any]?
+        do{
+            if let data = data {
+                jsonBody = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                
+            }else {
+                XCTFail("Bad data")
+            }
+          
+        }catch{
+            XCTFail("bad json string")
+            
+        }
+        
+        if let jsonBody = jsonBody{
+            print("jsonbody: \(jsonBody)")
+            let username: String = jsonBody["username"] as! String
+            let password: String = jsonBody["password"] as! String
+            let email : String = jsonBody["email"] as! String
+            XCTAssert(username == "Eddstah")
+            XCTAssert(password == "pass")
+            XCTAssert(email == "eddstah@hotmail.com")
+        }else{
+            XCTFail("Json body is nil")
+        }
+        
+        
+        
+       
         
         //Data should not be nil
         XCTAssert(data != nil)
-        
-        
     
+    }
+    
+    
+    
+    func testSignUp(){
+        let expecting = expectation(description: "Should return a token after call")
+        
+        let user = User(username: "Eddstah", email: "eddstah@hotmail.com", password: "pass")
+        
+        var triggered = false
+        UserAPI.signUp(new: user) { (token: String?, error: Error?) in
+            
+            if let token = token{
+                print("TOKEN: \(token)")
+            }else if let error = error{
+               print("ERROR: \(error)")
+                XCTFail()
+            }else {
+                XCTFail()
+            }
+            
+            expecting.fulfill()
+        }
+        
+        
+        waitForExpectations(timeout: 100) { (error: Error?) in
+            if let error = error{
+                 XCTFail("Took to long errors fam")
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
     func testPerformanceExample() {}

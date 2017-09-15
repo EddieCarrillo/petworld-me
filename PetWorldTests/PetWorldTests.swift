8e2d4d7782144580910e459a32fd04f8da6f139a
EddieCurio <edcarril@ucsd.edu>
2017-09-14 19:49:50 -0700

    Added query tests for contained in query operator. Also added setup function.

--- PetWorldTests/PetWorldTests.swift
+++ PetWorldTests/PetWorldTests.swift
@@ -12,14 +12,32 @@ import XCTest
 
 class PetWorldTests: XCTestCase {
     
+    var authToken: String?
+    
     override func setUp() {
         super.setUp()
+        var expecting = expectation(description: "To return an auth token")
+        
+        
+        let username = "JonRogawski"
+        let password = "Mathprof123"
+        AuthAPI.login(username: username, password: password) { (token:
+            String?, error:Error?) in
+            if let error = error {
+                print(error)
+            }else if let token = token{
+                self.authToken = token
+                expecting.fulfill()
+            }
+            
+        }
+        
+        waitForExpectations(timeout: 10, handler: { (error: Error?) in
+            if let error = error{
+                print("Request timed out")
+            }
+        })
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
     
     //Because
@@ -43,6 +61,31 @@ class PetWorldTests: XCTestCase {
         
         
     }
+    
+    
+    func testContainedInQuery(){
+        
+        let expecting = expectation(description: "Return all posts belonging to ")
+        let query = Query()
+        query.whereKey(key: "author", containedIn: ["59a4890ab3b81f130414779b"])
+        PostsAPI.getPosts(query: query) { (posts:[Post]?, error: Error?) in
+            if let error = error {
+                XCTFail("[ERROR: \(error)]")
+            }else if let posts = posts {
+                print("posts: \(posts)")
+                expecting.fulfill()
+            }
+        }
+        
+        
+        waitForExpectations(timeout: 5) { (error: Error?) in
+            print("[ERROR]: \(error)")
+        }
+    
+        
+    }
+    
+    
     
 }
 

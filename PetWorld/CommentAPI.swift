54b17f1325091f7487bc5e8aa1c50ee23b95bbfd
EddieCurio <edcarril@ucsd.edu>
2017-09-15 14:49:36 -0700

    Only using query now to get comments.

--- PetWorld/CommentAPI.swift
+++ PetWorld/CommentAPI.swift
@@ -14,9 +14,9 @@ class CommentAPI{
     static var postMethod = "POST"
     
     
-    class func getComments(from postId: String, queryParams: Query?, onFinished: @escaping([Comment]?, Error?) -> Void){
+    class func getComments(query: Query?,  onFinished: @escaping([Comment]?, Error?) -> Void){
         let url = "\(NetworkAPI.apiBaseUrl)\(path)"
-        GeneralNetworkAPI.get(urlString: url, token: nil, queryParams: queryParams) { (data: Data?, error: Error?) in
+        GeneralNetworkAPI.get(urlString: url, token: nil, queryParams: query) { (data: Data?, error: Error?) in
             if let error = error{
                 onFinished(nil, NSError(domain: "Couldn't create comment.", code: 404, userInfo: [:]))
                 return;
@@ -48,12 +48,8 @@ onFinished(nil, NSError(domain: "Couldn't create from json repsonse.", code: 404
     
     
     class func create(comment: Comment, token: String, for postId: String, onFinished: @escaping(Comment?, Error?) -> Void){
-        let jonRogawskiId = "59aef169256ec2132ea6a392"
-        let postId = "59aef4fd256ec2132ea6a39b"
-        let comment1 = Comment(text: "I am comment 1.", authorId: jonRogawskiId, postId: postId)
-//         let comment2 = Comment(text: "I am comment 1.", authorId: jonRogawskiId, postId: postId)
-//         let comment3 = Comment(text: "I am comment 1.", authorId: jonRogawskiId, postId: postId)
-        guard  let comment1Json = comment1.toJson() else {
+        
+        guard  let comment1Json = comment.toJson() else {
             onFinished(nil, NSError(domain: "Could convert to json", code: 404, userInfo: nil))
             return;
         }
@@ -82,12 +78,4 @@ onFinished(nil, NSError(domain: "Couldn't create from json repsonse.", code: 404
         }
     }
     
-    
-   
-    
-    
-    
-    
-    
-
 }
